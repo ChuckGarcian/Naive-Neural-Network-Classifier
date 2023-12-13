c625ba6d88b8073907de5f5b5c9e406b89a545ae
ChuckGarcian <chuckgarcian@gmail.com>
2023-12-12 18:17:33 -0600

    added matrix multiplication and addition operations

--- src/matrix.py
+++ src/matrix.py
@@ -7,30 +7,94 @@ Basic Matrix Class - Key Features:
     the dot product of the two matrices
 
 """
+def PANIC (msg):
+    raise Exception (msg);
 
 class Matrix:
-    def __init__(self, col, row):
+    def __init__(self, row, col):
         self.con = []
+        self.col =  col;
+        self.row = row;
         for r in range(row):
             entry = []
             for c in range(col):
                 entry.append(0)
             self.con.append(entry)
-
-    def mt_set (self, i, j, val):
-        self.con[i][j] = val;
+    def matrix_from_container (self, data):
+        self.con = data;
+        self.row = len (data);
+        self.col = len (data[0]);
     
+    ## Access Functions ##
+    def __setitem__ (self, k, val):
+        self.con[k] = val;
+    
+    def __getitem__ (self, k):
+        return self.con[k]
+    
+    ## Matrix Operation Functions ##
+    def __multiply__ (self, other):
+        res = Matrix (self.row, other.col);
+        
+        for r in range (self.row):
+            for c in range (other.col):
+                entry = 0;
+                for ra in range (self.col):
+                    entry += self.con[r][ra] * other.con[ra][c];
+                res[r][c] = entry;
+        
+        return res;
+
+    def __mul__ (self, other):
+        if (self.col != other.row or self.row != other.col):
+            PANIC ("Tensor Multiplication Collision");
+        else:
+            return self.__multiply__ (other);    
+            
+    def __add__ (self, other):
+        if (self.col != other.col or self.row != other.row):
+            PANIC ("Tensor Addition Collision - Attempted to add two different sized tensors ");
+        else:
+            res = Matrix (self.row, self.col);
+            for r in range (self.row):
+                for c in range (self.col):
+                    res[r][c] = self[r][c] + other[r][c];
+            
+            return res;
+
+    def ones (self):
+        for r in range (self.row):
+            for c in range (self.col):
+                self.con[r][c] = 1;
+                
     def __str__(self):
-        return self.con.__str__()
-
+        s = ''
+        # for r in self.data:
+        #     s += str(r) + '\n'
+        
+        for r in range (self.row):
+            s += str(self.con[r]) + '\n';
+        return s
+    
 # Test
-r = 10;
-c = 10;
-mat = Matrix (r,c);
+rows = 10
+cols = 10
+mat = Matrix(rows, cols)
 
-for i in range (r):
-    for j in range (c):
-        mat.mt_set (i, j, i);
+for r in range(rows):
+    for c in range(cols):
+        mat.con[r][c] = c
 
+r = 0;
+c = 1;
+print ("R=", r);
+print ("C=", c);
+print ("Entry mat=", mat[r][c]);
 print (mat);
+
+mat2 = Matrix (rows, cols);
+mat2.ones();
+mat3 = mat * mat2;
+print (mat3);
+
 
